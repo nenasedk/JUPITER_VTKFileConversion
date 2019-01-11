63262a46cc154c5e29c3101de120edc87a008580
Evert Nasedkin <nasedkinevert@gmail.com>
2019-01-11 15:50:18 +0100

    Got matplotlib images working. Use VTK to slice data, then plot with tricontourf

--- VTKtoNumpyPlotting.py
+++ VTKtoNumpyPlotting.py
@@ -3,10 +3,13 @@ from vtk.util.numpy_support import vtk_to_numpy
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from mpl_toolkits.mplot3d import Axes3D
+
+import matplotlib.colors as colors
 from scipy.interpolate import griddata 
 import sys
 
-
+AU = 1.496e+13
 VTK_DIR = "/home/evert/Documents/SemesterProject/output/opa_1jup_50AU/VTK00198/"
 VTK_FILE = "gasdensity198_5.vtk"
 
@@ -16,45 +19,62 @@ reader.SetFileName(VTK_DIR + VTK_FILE)
 reader.SetScalarsName("gasdensity")
 reader.Update() # Needed because of GetScalarRange
 output = reader.GetOutput()
-#cellData = output.GetCellData()
-#drange = cellData.GetRange()
+
 scalar_range = output.GetScalarRange()
 
-#cellCentersFilter = vtk.vtkCellCenters()
-#cellCentersFilter.SetInput(output)
-#cell
+# Select a Slice
+#create a plane to cut,here it cuts in the XZ direction (xz normal=(1,0,0);XY =(0,0,1),YZ =(0,1,0)
+plane=vtk.vtkPlane()
+plane.SetOrigin(0,0,1)
+plane.SetNormal(0,0,1)
 
-# Mesh Coordinates
-
-nodes_vtk_array = output.GetPoints().GetData()
+#create cutter
+cutter=vtk.vtkCutter()
+cutter.SetCutFunction(plane)
+cutter.SetInputConnection(reader.GetOutputPort())
+cutter.Update()
 
 # Data Array
-data_vtk_array = output.GetCells().GetData()#.GetScalars()
+data_vtk_array = cutter.GetOutput() #output.GetCells().GetData()#.GetScalars()
+
 mapper = vtk.vtkCellDataToPointData()
-mapper.AddInputData(output)
+mapper.AddInputData(data_vtk_array)
 mapper.Update()
 mapper_data = mapper.GetOutput()
 
+
+# Mesh Coordinates
+points = data_vtk_array.GetPoints()
+n_points = points.GetNumberOfPoints()
 # Node coordinates with data
-nodes_numpy_array = vtk_to_numpy(nodes_vtk_array)
-x,y,z = nodes_numpy_array[:,0], nodes_numpy_array[:,1], nodes_numpy_array[:,2]
-
+#nodes_numpy_array = vtk_to_numpy(data_vtk_array.GetPoints())
+x = np.zeros(n_points)
+y = np.zeros(n_points)
+z = np.zeros(n_points)
+for i in range(n_points):
+    pt = points.GetPoint(i)
+    x[i] = pt[0]
+    y[i] = pt[1]
+    #z[i] = pt[2]
+    
+x = x/AU
+y = y/AU
 data_numpy_array = vtk_to_numpy(mapper_data.GetPointData().GetArray(0))
-data = data_numpy_array
-
+npts = len(x)
+data = np.abs(data_numpy_array)
 # Contours!
-npts = 100
 xmin,xmax = min(x),max(x)
 ymin,ymax = min(y),max(y)
 
 # Grid
-xi = np.linspace(xmin, xmax, npts)
-yi = np.linspace(ymin, ymax, npts)
-data_grid = griddata((x,y), data, (xi[None,:], yi[:,None]), method = 'cubic')
+data_grid = griddata((x,y), data, (x,y), method = 'nearest')
+lvls = np.logspace(data.min(),data.max(),16)
 
-# Contour Plot
-CS = plt.contour(xi,yi,data_grid, linewidths = 3, cmap = cm.jet)
-plt.clabel(CS, inline=1,inline_spacing= 3, fontsize=12, colors='k', use_clabeltext=1)
+plt.tricontourf(x,y,np.log10(data),255,)#, lvls)#cmap = cm.plasma, norm = colors.LogNorm())
+plt.title("Log of Gas Density of circumstellar disk around a 1 Jupiter mass planet at 1AU")
+plt.xlabel("x [AU]")
+plt.ylabel("y [AU]")
 
-plt.colorbar()
+cbar = plt.colorbar()
+cbar.set_label("Log of Gas Density [$g/cm^{3}$]")
 plt.show()
